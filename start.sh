41cdaaca37a98d0ad8910919264b04b3cefec10f
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-25 15:30:59 -0700

    add container entry script

---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# start docker container with xorg windows and more
+
+xhost +local:$USERNAME
+export ROS_DOMAIN_ID=0
+
+if [ "$(docker ps -aq -f name=airos)" ]
+then
+  if ["$(docker ps -q -f name=airos)" ]
+  then
+    echo "container is running. attaching..."
+  else
+    echo "starting existing container"
+    docker start airos
+  fi
+  echo "entering existing container"
+  docker exec -it \
+      -e DISPLAY=$DISPLAY \
+      -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
+      airos bash
+else
+  echo "creating container from image"
+  docker run -it \
+      --name airos \
+      --env DISPLAY=$DISPLAY \
+      --env ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
+      --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+      --volume="$HOME/.ssh:/home/ros/.ssh:ro" \
+      --volume="$HOME/.gitconfig:/home/ros/.gitconfig:ro" \
+      --device=/dev/dri \
+      --group-add video \
+      --net=host \
+      --privileged \
+      ros2 bash
+fi
+
+
